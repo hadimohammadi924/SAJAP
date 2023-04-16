b524e6e3789c34a2282b09583defffc60701cf4f
uuTT <hadimohammadi924@gmail.com>
2023-04-16 22:35:35 +0330

    hod

--- app/src/main/java/ir/hadimohammadi/myapplication/MainActivity.java
+++ app/src/main/java/ir/hadimohammadi/myapplication/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-String hadi="hadi";
+
 
         ahkam = findViewById(R.id.ahkam);
         tashvigat = findViewById(R.id.tashvigat);
