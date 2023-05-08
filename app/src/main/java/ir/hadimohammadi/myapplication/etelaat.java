d60ef581df06397c2742500edc8225bdf52a8af3
forosh <hadimohammadi924@gmail.com>
2023-05-08 15:39:26 +0330

    ETELAATCARD INSERT

---
+++ app/src/main/java/ir/hadimohammadi/myapplication/etelaat.java
@@ -0,0 +1,104 @@
+package ir.hadimohammadi.myapplication;
+
+
+import com.google.gson.annotations.SerializedName;
+
+public class etelaat {
+
+    @SerializedName("id")
+    private int id;
+
+    @SerializedName("type")
+    private String type;
+
+
+    @SerializedName("date")
+    private String date;
+
+    @SerializedName("type1")
+    private String type1;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getType1() {
+        return type1;
+    }
+
+    public void setType1(String type1) {
+        this.type1 = type1;
+    }
+
+    public String getType2() {
+        return type2;
+    }
+
+    public void setType2(String type2) {
+        this.type2 = type2;
+    }
+
+    public String getType3() {
+        return type3;
+    }
+
+    public void setType3(String type3) {
+        this.type3 = type3;
+    }
+
+    public String getType4() {
+        return type4;
+    }
+
+    public void setType4(String type4) {
+        this.type4 = type4;
+    }
+
+    public String getType5() {
+        return type5;
+    }
+
+    public void setType5(String type5) {
+        this.type5 = type5;
+    }
+
+    @SerializedName("type2")
+    private String type2;
+
+    @SerializedName("type3")
+    private String type3;
+
+    @SerializedName("type4")
+    private String type4;
+
+
+    @SerializedName("type5")
+    private String type5;
+
+
+
+    public etelaat() {
+    }
+
+
+}
