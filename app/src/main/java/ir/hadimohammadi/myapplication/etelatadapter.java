f71958b3bcee24ea394168c65845a295137d6ee0
forosh <hadimohammadi924@gmail.com>
2023-05-09 14:36:20 +0330

    tablo select

---
+++ app/src/main/java/ir/hadimohammadi/myapplication/etelatadapter.java
@@ -0,0 +1,146 @@
+package ir.hadimohammadi.myapplication;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class etelatadapter extends RecyclerView.Adapter<etelatadapter.ContactViewHolder> {
+
+    Context mConext;
+    public SharedPreferences sp;
+    private List<etelaat> etelatList;
+    public static Activity context;
+
+    static etelatadapter etelatAdapterInterface;
+
+    public etelatadapter(ArrayList<etelaat> etelatlist, etelatadapter.etelatAdapterInterface etelatAdapterInterface) {
+        this.etelatList = etelatlist;
+        this.etelatAdapterInterface = (etelatadapter) etelatAdapterInterface;
+
+    }
+
+
+    public interface etelatAdapterInterface {
+
+        void onCustomListitemClick(int position);
+    }
+
+    public etelatadapter(List<etelaat> etelatList, Activity activity) {
+        this.etelatList = etelatList;
+        context = activity;
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return etelatList.size();
+    }
+
+
+    @Override
+    public void onBindViewHolder(etelatadapter.ContactViewHolder contactViewHolder, @SuppressLint("RecyclerView") int i) {
+        //   SharedPreferences sp = mConext.getApplicationContext().getSharedPreferences("SAJAP", 0);
+        //   sp.getString("post","");
+        //   Toast.makeText(mConext,  sp.getString("post",""), Toast.LENGTH_SHORT).show();
+
+        if (etelatList.get(i).getType().equals("1")) {
+
+
+            contactViewHolder.eteladate.setText("اطلاعیه مورخه :" + etelatList.get(i).getDate());
+            contactViewHolder.etelatitle.setText("عنوان: " + etelatList.get(i).getType1());
+            contactViewHolder.etelaperiod.setText("عنوان شکایت: " + etelatList.get(i).getType2());
+            contactViewHolder.type3.setText("حوزه شکایت: " + etelatList.get(i).getType3());
+            contactViewHolder.type4.setText("ثبت شده توسط:" + etelatList.get(i).getType4());
+
+
+        }
+        else
+        {
+            ContactViewHolder.loggo.setVisibility(View.GONE);
+            contactViewHolder.eteladate.setText("اطلاعیه مورخه :" + etelatList.get(i).getDate());
+            contactViewHolder.etelatitle.setText("عنوان: " + etelatList.get(i).getType1());
+            contactViewHolder.etelaperiod.setText("عنوان شکایت: " + etelatList.get(i).getType2());
+            contactViewHolder.type3.setText("حوزه شکایت: " + etelatList.get(i).getType3());
+            contactViewHolder.type4.setText("ثبت شده توسط:" + etelatList.get(i).getType4());
+
+            ContactViewHolder.tabb.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context, tablo.class);
+                  //  context.startActivity(intent);
+
+               //     String hh =etelatList.get(contactViewHolder.getAdapterPosition(i))+"";
+
+
+
+
+
+                }
+            });
+
+
+
+        }
+
+    }
+
+    @Override
+    public ContactViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.etelaatcard, viewGroup, false);
+        return new ContactViewHolder(itemView);
+    }
+
+    public static class ContactViewHolder extends RecyclerView.ViewHolder {
+
+
+        static CardView tabb, loggo;
+        TextView eteladate, etelatitle, etelaperiod, type3, type4;
+        LottieAnimationView maill, showw;
+
+        public ContactViewHolder(final View v) {
+            super(v);
+
+
+            eteladate = v.findViewById(R.id.eteladate);
+            etelatitle = v.findViewById(R.id.etelatitle);
+            etelaperiod = v.findViewById(R.id.etelaperiod);
+            type3 = v.findViewById(R.id.type3);
+            type4 = v.findViewById(R.id.type4);
+            maill = v.findViewById(R.id.maill);
+            showw = v.findViewById(R.id.showw);
+            loggo = v.findViewById(R.id.loggo);
+            tabb = v.findViewById(R.id.tabb);
+
+
+        }
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
--- app/src/main/java/ir/hadimohammadi/myapplication/tablo.java
+++ app/src/main/java/ir/hadimohammadi/myapplication/tablo.java
@@ -1,6 +1,7 @@
 package ir.hadimohammadi.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
@@ -8,13 +9,32 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import saman.zamani.persiandate.PersianDate;
 import saman.zamani.persiandate.PersianDateFormat;
 
 public class tablo extends AppCompatActivity {
 
-    RecyclerView ticketsListRecyclerView;
+    RecyclerView tabloListRecyclerView;
     TextView xtt2;
     PersianDate pdate = new PersianDate();
     PersianDateFormat pdformater1 = new PersianDateFormat("Y/m/d");
@@ -25,18 +45,86 @@ public class tablo extends AppCompatActivity {
         setContentView(R.layout.activity_tablo);
         tcast();
 
+        String tiketurl = "https://pgtab.ir/home/ettelaat";
+
+
+        RequestQueue requestQueue;
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+        Network network = new BasicNetwork(new HurlStack());
+        requestQueue = new RequestQueue(cache, network);
+        requestQueue.start();
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, tiketurl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String responsee) {
+                        setUpRecyclerView(responsee);
+                        Toast.makeText(tablo.this, responsee, Toast.LENGTH_SHORT).show();
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+        requestQueue.add(stringRequest);
 
 
     }
 
 
+
     public void tcast() {
 
         xtt2 = findViewById(R.id.xtt2);
+        tabloListRecyclerView = findViewById(R.id.tabloListRecyclerView);
 
 
         SharedPreferences sharedPreferences = getSharedPreferences("SAJAP", Context.MODE_PRIVATE);
         xtt2.setText("امروز مورخ : "+pdformater1.format(pdate));
+    }
+
+    public List<etelaat> createlist2(String responsee) {
+
+        List<etelaat> lst2 = new ArrayList<>();
+
+
+        try {
+            JSONArray jsonArray = new JSONArray(responsee);
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                etelaat info = new etelaat();
+                JSONObject ttemp = jsonArray.getJSONObject(i);
+                info.setId(ttemp.getInt("id"));
+                info.setType(ttemp.getString("type"));
+                info.setDate(ttemp.getString("date"));
+                info.setType1(ttemp.getString("type1"));
+                info.setType2(ttemp.getString("type2"));
+                info.setType3(ttemp.getString("type3"));
+                info.setType4(ttemp.getString("type4"));
+
+
+
+                lst2.add(info);
+            }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return lst2;
+
+    }
+
+    public void setUpRecyclerView(String responsee) {
+        LinearLayoutManager layout = new LinearLayoutManager(tablo.this, LinearLayoutManager.VERTICAL, false);
+        tabloListRecyclerView.setLayoutManager(layout);
+
+        etelatadapter eadpter = new etelatadapter(createlist2(responsee), tablo.this);
+        tabloListRecyclerView.setAdapter(eadpter);
+
     }
 
 
