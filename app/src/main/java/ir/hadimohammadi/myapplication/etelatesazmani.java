a5de056575afd6c04e5f983f9fa90c75c8f40faa
uuTT <hadimohammadi924@gmail.com>
2023-06-12 00:33:17 +0330

    det

--- app/src/main/java/ir/hadimohammadi/myapplication/etelatesazmani.java
+++ app/src/main/java/ir/hadimohammadi/myapplication/etelatesazmani.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,7 +30,7 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class etelatesazmani extends AppCompatActivity {
     SharedPreferences sp;
-    TextView personall;
+    TextView personall,codemelli,codemellii,shenasname,shenasnamee,tell,telll,mobile,mobilee,madrakk,madrak,reshtetahsili,reshtetahsilii;
     CircleImageView profile_imagee;
 
 
@@ -39,6 +41,18 @@ public class etelatesazmani extends AppCompatActivity {
         sp = getApplicationContext().getSharedPreferences("SAJAP", Context.MODE_PRIVATE);
         personall = findViewById(R.id.personall);
         profile_imagee = findViewById(R.id.profile_imagee);
+        codemelli = findViewById(R.id.codemelli);
+        codemellii = findViewById(R.id.codemellii);
+        shenasname = findViewById(R.id.shenasname);
+        shenasnamee = findViewById(R.id.shenasnamee);
+        tell = findViewById(R.id.tell);
+        telll = findViewById(R.id.telll);
+        mobile = findViewById(R.id.mobile);
+        mobilee = findViewById(R.id.mobilee);
+        madrakk = findViewById(R.id.madrakk);
+        madrak = findViewById(R.id.madrak);
+        reshtetahsili = findViewById(R.id.reshtetahsili);
+        reshtetahsilii = findViewById(R.id.reshtetahsilii);
 
     Glide
             .with(etelatesazmani.this)
@@ -48,8 +62,19 @@ public class etelatesazmani extends AppCompatActivity {
             .into(profile_imagee);
 
    personall.setText(sp.getString("Users_Name", "") + " " + sp.getString("Users_FName", ""));
+   /*
+        telll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneno="09360610067";
 
 
+                startActivity(new Intent(Intent.ACTION_DIAL,Uri.parse(phoneno)));
+            }
+        });
+
+
+    */
         sdata();
 
 
@@ -68,7 +93,17 @@ public class etelatesazmani extends AppCompatActivity {
                     public void onResponse(String response) {
                         try {
                             JSONObject respo = new JSONObject(response);
-                            Toast.makeText(etelatesazmani.this, respo.getString("madrak"), Toast.LENGTH_SHORT).show();
+                            codemellii.setText(respo.getString("codemelli"));
+                            shenasnamee.setText(respo.getString("shenasname"));
+                            telll.setText(respo.getString("tell"));
+                            mobilee.setText(respo.getString("mobile"));
+                            madrakk.setText(respo.getString("madrak"));
+                            reshtetahsilii.setText(respo.getString("reshtetahsili"));
+
+
+
+
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                             Toast.makeText(etelatesazmani.this, "2", Toast.LENGTH_SHORT).show();
